31b042191bb2fb01d559e2c07075015f16631e86
koki inoue <ko.18.5in@gmail.com>
2020-06-07 02:22:35 +0900

    add space

--- main.go
+++ main.go
@@ -1,34 +1,26 @@
-// 要件
-
-// Stringerインタフェース
-// String() stringメソッドを持つインタフェースを作る
-// そして3つ以上Stringerインタフェースを実装する型を作る
-
-// インタフェースを受け取る関数
-// Stringerインタフェースを引数で受け取る関数を作る
-// 受け取った値を上記の3つの具象型によって分岐し、具象型の型名と値を表示する
 package main
 
 import "fmt"
 
-// ここでインタフェースを作成
 type Stringer interface {
 	String() string
 }
 
-// typeで
 type I int
+
 func (n I) String() string {
 	return "I"
 }
 
 type B bool
+
 func (n B) String() string {
 	return "B"
 }
 
 type S string
-func(n S) String() string {
+
+func (n S) String() string {
 	return "S"
 }
 
